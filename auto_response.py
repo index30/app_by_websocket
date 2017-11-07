1ea673bed4e4e2965661b0bc4e7ed2eda3562216
ranthy <realthingshakes25@gmail.com>
2017-11-08 00:53:32 +0900

    ジャンルに応じた固定の提案を追加. コードが煩雑化しているので要修正

--- auto_response.py
+++ auto_response.py
@@ -6,7 +6,6 @@ class DefaultResponse():
     def __init__(self):
         pass
 
-    #word2vecを使いたい(方針)
     def wakati_node_parse(self, message):
         m = MeCab.Tagger("-Ochasen")
         parse_txt = m.parseToNode(message)
@@ -19,6 +18,12 @@ class DefaultResponse():
                 node_dic[part_speech[0]] = node_dic[part_speech[0]] + [node]
             parse_txt = parse_txt.next
         return node_dic
+
+    def return_genre_sentence(self, word):
+        dish_genre = ['中華', 'インド', '和食', '洋食']
+        corr_sentence = ['まーボーどうふ', 'カレー', 'うどん', 'ハンバーグ']
+        match_list = [corr_sentence[i] for (i, g) in enumerate(dish_genre) if g in word]
+        return match_list
 
     def response_genre(self, word):
         topic = ['スポーツ', '料理', '政治']
@@ -43,7 +48,7 @@ class DefaultResponse():
             return_mes = "よくわかりませんでした...もういちどおねがいできますか?"
             return return_mes
         elif genre == "未知":
-            return_mes = "その単語は知らないです...ごめんなさい"
+            return_mes = "そのたんごはしらないです...ごめんなさい"
             return return_mes
         else:
             return_mes = "それはりょうりですか??"
@@ -57,8 +62,12 @@ class DefaultResponse():
         noun = self.remove_empty_ele(wakati_node["名詞"])
         if noun:
             try:
-                genre = self.response_genre(noun)
-                return self.return_template(message, genre)
+                match = self.return_genre_sentence(noun)
+                if match:
+                    return "それでは"+"か".join(match)+"はいかがでしょう?"
+                else:
+                    genre = self.response_genre(noun)
+                    return self.return_template(message, genre)
             except:
                 return self.return_template(message, "未知")
         else:
